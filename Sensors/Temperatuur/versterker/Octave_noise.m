f082214dc079d4a4b1d975e834e9b9355c6ef955
Niek_laptop_arch <niek.louwerse@hva.nl>
2024-09-30 20:52:11 +0200

    added an if boom

--- Sensors/Temperatuur/versterker/Octave_noise.m
+++ Sensors/Temperatuur/versterker/Octave_noise.m
@@ -15,17 +15,30 @@ frequentie = logspace(-1, 7, 100000);
 
 %k = physconst('Boltzmann');
 k = 1.380649e-23;  % Boltzmann constant in J/K
-c = 23;
-T = c + 273;
+c = 55;
+T = c + 273,15;
 
 % source impedance %
-R_source = 100e3;
+R_source = 12e3;
 source_impedance = R_source;
+
+if frequentie <= 10
+    one_over_f_noise_current_opamp = 260e-9;
+elseif frequentie <= 100
+    one_over_f_noise_current_opamp = 85e-9;  % 85 nV/√Hz
+elseif frequentie <= 1000
+    one_over_f_noise_current_opamp = 85e-9 * exp(-0.002 * (frequentie - 100));  % exponential drop between 100 and 1kHz
+else
+    one_over_f_noise_current_opamp = 30e-9 * exp(-0.001 * (frequentie - 1000));  % further decay beyond 1kHz
+end
+% dit laten checken door stijn
+noise_current_opamp_current = 0;
+
 
 % Pre-amp %
 %overdracht amplifier
-gain = 50.40; %versterking van amplifier
-fc = 340e3; %cutoff frequentie amplifier
+gain = 52; %versterking van amplifier
+fc = 350e3; %cutoff frequentie amplifier
 
 R = 1;
 C = 1 / (2 * pi * R * fc);
@@ -46,7 +59,7 @@ voltage_noise_amplifier = sqrt((one_over_F_voltage_noise).^2 + (broadband_voltag
 
 %Equivalent Current Input Noise from amplifier
 current_noise_amplifier = 1e-15;
-voltgage_current_noise = current_noise_amplifier .* source_impedance;
+voltage_current_noise = current_noise_amplifier .* source_impedance;
 
 %Voltage Input Noise from Feed_back_resitor
 Feed_back_resitor = 972;
@@ -56,7 +69,7 @@ Noise_Feed_back_resitor = sqrt(4 * k * T * Feed_back_resitor);
 Input_Noise = sqrt(4 * k * T * R_source);
 
 %total Voltage Input Noise from amplifier
-total_voltage_noise_input = sqrt((voltage_noise_amplifier).^2 + (voltgage_current_noise).^2 + (Input_Noise).^2 + (Noise_Feed_back_resitor).^2);
+total_voltage_noise_input = sqrt((voltage_noise_amplifier).^2 + (voltage_current_noise).^2 + (Input_Noise).^2 + (Noise_Feed_back_resitor).^2);
 
 %output noise amplifier
 output_noise_amplifier = total_voltage_noise_input .* transfer_amplifier;
